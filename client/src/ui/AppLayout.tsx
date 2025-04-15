cbb3b97dda7f31d0887095c96812954852e6d061
Emir Yscn <emiryasacan21@gmail.com>
2025-04-15 23:01:15 +0300

    ui changes

--- client/src/ui/AppLayout.tsx
+++ client/src/ui/AppLayout.tsx
@@ -12,40 +12,33 @@ function AppLayout() {
 
   return (
     // Container for centering with padding
-    <div className="min-h-screen  flex justify-center p-4 md:p-8">
+    <div className="min-h-screen flex justify-center md:p-8">
       {/* App container with max-width and rounded corners */}
-      <div className="w-full max-w-99/100 rounded-2xl overflow-hidden flex flex-col md:flex-row relative bg-[var(--color-grey-50)] text-[var(--color-grey-700)] shadow-lg">
-        {/* Main content area (flex on mobile, grid on larger screens) */}
-        <div className="flex-1 flex flex-col md:grid md:grid-cols-[auto_1fr] lg:grid-cols-[400px_1fr]">
-          {/* Chats section - hidden by default on small screens unless toggled */}
-          <div
-            className={`
-              ${showChats ? "block" : "hidden"}
-              md:block
-              transition-all duration-300
-              h-[calc(100vh-9rem)] md:h-[calc(100vh-4rem)] lg:h-[calc(100vh-4rem)] overflow-y-auto
-            `}
-          >
-            <Chats onToggleChats={handleToggleChats} />
-          </div>
-
-          {/* Main content - visible by default on small screens unless chats toggled */}
-          <main
-            className={`
-              ${showChats ? "hidden" : "block"} 
-              md:block
-              h-[calc(100vh-9rem)] md:h-[calc(100vh-4rem)] lg:h-[calc(100vh-4rem)] overflow-y-auto
-            `}
-          >
-            <Outlet />
-          </main>
-        </div>
-
-        {/* Sidebar - fixed at bottom on small/medium screens, left side on large screens */}
-
-        <div className="w-full lg:w-auto h-full rounded-xl lg:rounded-none lg:rounded-l-2xl overflow-hidden">
+      <div className="w-full md:max-w-99/100 md:rounded-2xl overflow-hidden grid grid-rows lg:grid lg:grid-cols-[auto_auto_1fr] relative bg-[var(--color-grey-50)] text-[var(--color-grey-700)] shadow-lg">
+        <div className="w-full lg:w-auto h-full rounded-xl lg:rounded-none lg:rounded-l-2xl overflow-hidden order-last lg:order-first border-t-2 border-[var(--color-grey-100)] lg:border-t-0 lg:border-r-2 ">
           <Sidebar onToggleChats={handleToggleChats} showChats={showChats} />
         </div>
+        <div
+          className={`
+              ${showChats ? "block" : "hidden"}
+              lg:block
+              transition-all duration-300
+              h-[calc(100vh-9rem)]  lg:h-[calc(100vh-4rem)] overflow-y-auto
+              lg:border-[var(--color-grey-100)] lg:border-r-2 
+            `}
+        >
+          <Chats onToggleChats={handleToggleChats} />
+        </div>
+
+        <main
+          className={`
+              ${showChats ? "hidden" : "block"} 
+              lg:block
+              h-[calc(100vh-9rem)]  lg:h-[calc(100vh-4rem)] overflow-y-auto
+            `}
+        >
+          <Outlet />
+        </main>
       </div>
     </div>
   );
--- client/src/ui/Searchbar.tsx
+++ client/src/ui/Searchbar.tsx
@@ -1,16 +1,21 @@
-import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { FaArrowLeft, FaSearch } from "react-icons/fa";
 
 function Searchbar() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
-    <div className="flex gap-4 items-center bg-red-500 px-4 py-1 rounded-2xl text-white">
+    <div className="flex gap-4 items-center bg-[var(--color-grey-100)] px-4 py-1 rounded-2xl ">
       <label htmlFor="searchbar">
-        <FaSearch />
+        {searchValue ? <FaArrowLeft /> : <FaSearch />}
       </label>
       <input
         type="search"
         name="searchbar"
         id="searchbar"
         placeholder="Search..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
         className="w-full focus:outline-none"
       />
     </div>
